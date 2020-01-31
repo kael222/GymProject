8405718e20073a1ecbf1095433074967f51b85ff
Łukasz Kowalik <lukaszkowalik25@gmail.com>
2020-01-31 21:27:16 +0100

    LandingPage simple with simple slider

--- scripts/silka.js
+++ scripts/silka.js
@@ -1,72 +1,51 @@
-// // zaczate storera jakb yco
+// zaczate storera jakb yco
 
-// let buttons = document.getElementById("rododendron");
-// console.log(buttons);
+let buttons = document.getElementById("rododendron");
+console.log(buttons);
 let buttons1 = document.getElementById("rododendron1");
-// console.log(buttons1);
-
-// let pp = document.getElementById("licz");
-// console.log(pp);
-
-// let stopCount;
-
-// let count = 0;
-
-// buttons.addEventListener("click", function() {
-//   stopCount = setInterval(function() {
-//     count += 1;
-//     pp.textContent = count;
-//     console.log(count);
-//   }, 1000);
-// });
-
-// buttons1.addEventListener("click", function() {
-//   clearInterval(stopCount);
-// });
-
-// // Próbuję stworzyc listę to do do ćwiczeń
-
-// let addButtons = document.getElementById("addExercise");
-// console.log(addButtons);
-
-// let lists = document.getElementById("list");
-// console.log(lists); //ul
-
-// addButtons.addEventListener("click", function() {
-//   let addInput = document.getElementById("kupa");
-//   console.log(addInput);
-
-//   let newli = document.createElement("li"); //tworzę lsitę li
-//   let paragrph = document.createElement("p");
-//   let b1 = document.createElement("button");
-//   let b2 = document.createElement("button");
-
-//   b1.innerText = "completed";
-//   b2.innerText = "deleted";
-//   paragrph.innerText = addInput.innerHTML;
-//   lists.appendChild(newli);
-//   newli.appendChild(paragrph);
-//   newli.appendChild(b1);
-//   newli.appendChild(b2);
-// });
+console.log(buttons1);
 
 let pp = document.getElementById("licz");
-var licz = 0;
-var sprzątajkuchnię;
-let buttons = document.getElementById("rododendron");
+console.log(pp);
+
+let stopCount;
+
+let count = 0;
+
 buttons.addEventListener("click", function() {
-  sprzątajkuchnię = setInterval(function() {
-    pp.textContent = licz;
-    pp.innerText =
-      "sprzątaj kuchnię albo kasa " + licz + " złotych do skarbonki";
-    licz += 1;
-    console.log("sprzątaj kuchnię albo kasa " + licz + " groszy do skarbonki");
+  stopCount = setInterval(function() {
+    count += 1;
+    pp.textContent = count;
+    console.log(count);
   }, 1000);
 });
-buttons1.addEventListener(
-  "click",
-  function() {
-    clearInterval(sprzątajkuchnię);
-  },
-  2000
-);
+
+buttons1.addEventListener("click", function() {
+  clearInterval(stopCount);
+});
+
+// Próbuję stworzyc listę to do do ćwiczeń
+
+let addButtons = document.getElementById("addExercise");
+console.log(addButtons);
+
+let lists = document.getElementById("list");
+console.log(lists); //ul
+
+addButtons.addEventListener("click", function() {
+  let addInput = document.getElementById("kupa");
+  console.log(addInput);
+
+  let newli = document.createElement("li"); //tworzę lsitę li
+  let paragrph = document.createElement("p");
+  let b1 = document.createElement("button");
+  let b2 = document.createElement("button");
+
+  b1.innerText = "completed";
+  b2.innerText = "deleted";
+  paragrph.innerText = addInput.innerHTML;
+  lists.appendChild(newli);
+  newli.appendChild(paragrph);
+  newli.appendChild(b1);
+  newli.appendChild(b2);
+});
--- scripts/stronaStartowa.js
+++ scripts/stronaStartowa.js
@@ -7,3 +7,24 @@ hamburger.addEventListener("click", function() {
   hamburger.classList.toggle("hamburger-active");
   nav.classList.toggle("navigation-active");
 });
+
+//Slider
+
+var slideIndex = 0;
+
+function slideshow() {
+  var slide = document.getElementsByClassName("slides");
+  for (let i = 0; i < slide.length; i++) {
+    slide[i].style.display = "none";
+  }
+  slideIndex++;
+  // for (let i = 0; i < slide.length; i++) {
+  //   slide[i].style.display = "none";
+  // }
+  if (slideIndex > slide.length) {
+    slideIndex = 1;
+  }
+  slide[slideIndex - 1].style.display = "block";
+  setTimeout(slideshow, 3000);
+}
+slideshow();
